7f54c06c3b1906ed3f9c2e3a4a5d6d7c86f04649
Elgun837 <elgun.xalilov@bk.ru>
2021-12-20 17:55:30 +0400

    view share method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            
+        $settings = Setting::find(1);
+        $view->with('settings', $settings);
+           
+           
+            
+            
+     });
     }
 }
